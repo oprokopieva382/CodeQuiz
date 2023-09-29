0652cd9031d3698b4651b91c11af8554ba12d245
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-09-29 00:26:47 -0500

    start work with startQuestion function

--- script.js
+++ script.js
@@ -59,3 +59,27 @@ const questions = [
     ],
   },
 ];
+const toDisplayQuestion = document.getElementById("toDisplayQuestion");
+const startQuiz = document.getElementById("startQuiz");
+const text = document.getElementById("text");
+
+let error = "";
+let currentQuestion = 0;
+
+const startQuestion = () => {
+  toDisplayQuestion.innerHTML = questions[currentQuestion].question;
+  startQuiz.remove();
+  text.textContent = "";
+
+  for (let i = 0; i < questions[currentQuestion].answer.length; i++) {
+    let buttonOption = document.createElement("button");
+    buttonOption.className = questions[currentQuestion].answer[i].isCorrect;
+    buttonOption.textContent = questions[currentQuestion].answer[i].text;
+
+    let buttonDiv = document.createElement("div");
+    buttonDiv.appendChild(buttonOption);
+    text.appendChild(buttonDiv);
+  }
+};
+
+startQuiz.addEventListener("click", startQuestion);
