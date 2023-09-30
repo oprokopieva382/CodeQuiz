bcf424a1246c0fe00eba09ec9320e14641b7b9c9
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-09-30 18:54:13 -0500

    create timer and high record html page

--- script.js
+++ script.js
@@ -12,19 +12,44 @@ const resultText = document.getElementById("resultText");
 const submitButton = document.getElementById("submit");
 const initials = document.getElementById("initials");
 
-let message = "";
-console.log(message);
 let currentQuestion = 0;
-let timeLeft = 60;
+let timeLeft;
 
-const storeInitials = (e) => {
+
+
+//start timer when quiz runs
+const startTimer = () => {
+  console.log("tick tack start");
+  timeLeft = 75;
+
+  let timer = setInterval(() => {
+    timeLeft--;
+    time.textContent = `Time: ${timeLeft}`;
+    console.log("tick tack--");
+
+    if (timeLeft === 0) {
+      clearInterval(timer);
+      console.log("tick tack stop 0");
+      endQuiz();
+    }
+
+    if (currentQuestion >= questions.length) {
+      console.log("tick tack stop questions length");
+      clearInterval(timer);
+      endQuiz();
+    }
+  }, 1000);
+};
+
+//store player initials and score in local storage
+const storeData = (e) => {
   console.log("storeInitials start");
   e.preventDefault();
   let data = {
     playerInitials: initials.value.trim(),
-    playerScore: timeLeft
+    playerScore: timeLeft,
   };
-   console.log(data);
+  console.log(data);
   if (!initials.value.trim().length) {
     console.log("You have to enter your initials❗");
     resultMessageAlert.textContent = "❗You have to enter your initials❗";
@@ -41,10 +66,22 @@ const endQuiz = () => {
   seeResultSection.style.display = "flex";
   seeResultSection.style.height = "80vh";
   resultText.innerHTML = `🎉Your final score is: ${timeLeft}🎉`;
-  //seeResultButton.style.display = "block";
 };
 
-//run quiz question
+// function to check the selected answer
+const checkAnswer = (e) => {
+  const element = e.target;
+  console.log("checkAnswer");
+  if (element.classList.contains("true")) {
+    messageAlert.innerHTML = "That is exactly correct🤗";
+  } else {
+    messageAlert.innerHTML = "Unfortunately wrong, bro🫢";
+  }
+  currentQuestion++;
+  runQuestion();
+};
+
+// run quiz question function
 const runQuestion = () => {
   if (currentQuestion >= questions.length) {
     endQuiz();
@@ -53,22 +90,6 @@ const runQuestion = () => {
     toDisplayQuestion.innerHTML = questions[currentQuestion].question;
     startQuiz.remove();
     text.textContent = "";
-    time.innerHTML = `Time: ${timeLeft}`;
-
-    // Function to check the selected answer
-    const checkAnswer = (e) => {
-      const element = e.target;
-      console.log("checkAnswer");
-      if (element.classList.contains("true")) {
-        messageAlert.innerHTML = "That is exactly correct🤗";
-        console.log(message);
-      } else {
-        messageAlert.innerHTML = "Unfortunately wrong, bro🫢";
-        console.log(message);
-      }
-      currentQuestion++;
-      runQuestion();
-    };
 
     //run through the answer array to create and display buttons with content
     for (let i = 0; i < questions[currentQuestion].answer.length; i++) {
@@ -87,5 +108,11 @@ const runQuestion = () => {
   }
 };
 
-startQuiz.addEventListener("click", runQuestion);
-submitButton.addEventListener("click", storeInitials);
+//start quiz, run question, start timer
+const startQuizRun = () => {
+  startTimer();
+  runQuestion();
+};
+
+startQuiz.addEventListener("click", startQuizRun);
+submitButton.addEventListener("click", storeData);
