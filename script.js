52599b23ecd1d2c1a83150f4346b06cc55a50e68
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-09-29 22:34:53 -0500

    add style to answer buttons

--- script.js
+++ script.js
@@ -9,20 +9,21 @@ let currentQuestion = 0;
 let timeLeft = 0
 
 const startQuestion = () => {
-  console.log("Yo")
+  console.log("startQuestion");
   toDisplayQuestion.innerHTML = questions[currentQuestion].question;
   startQuiz.remove();
   text.textContent = "";
 
   for (let i = 0; i < questions[currentQuestion].answer.length; i++) {
     let buttonOption = document.createElement("button");
-    buttonOption.className = questions[currentQuestion].answer[i].isCorrect;
+    buttonOption.classList.add(questions[currentQuestion].answer[i].isCorrect);
     buttonOption.textContent = questions[currentQuestion].answer[i].text;
 
     let buttonDiv = document.createElement("div");
     buttonDiv.appendChild(buttonOption);
     text.appendChild(buttonDiv);
-  }
+
+   }
 };
 
 startQuiz.addEventListener("click", startQuestion);
