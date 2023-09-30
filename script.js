0da6fb149c207f93ca8cfc4052c475da3c26f9ea
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-09-29 22:12:03 -0500

    add state and change script file as module

--- script.js
+++ script.js
@@ -1,72 +1,15 @@
-const questions = [
-  {
-    question: "What type of data does not exist in JS?",
-    answer: [
-      {
-        text: "string",
-        isCorrect: false,
-      },
-      {
-        text: "number",
-        isCorrect: false,
-      },
-      {
-        text: "null",
-        isCorrect: false,
-      },
-      {
-        text: "thread",
-        isCorrect: true,
-      },
-    ],
-  },
-  {
-    question: "What example are the built-in methods in JS?",
-    answer: [
-      {
-        text: "slip()",
-        isCorrect: false,
-      },
-      {
-        text: "hop()",
-        isCorrect: false,
-      },
-      {
-        text: "pop()",
-        isCorrect: true,
-      },
-      {
-        text: "strim()",
-        isCorrect: false,
-      },
-    ],
-  },
-  {
-    question: "What is not the scopes of variable in JS?",
-    answer: [
-      {
-        text: "global scope",
-        isCorrect: false,
-      },
-      {
-        text: "local scope",
-        isCorrect: false,
-      },
-      {
-        text: "regional scope",
-        isCorrect: true,
-      },
-    ],
-  },
-];
+import {questions} from "./store/state.js";
+
 const toDisplayQuestion = document.getElementById("toDisplayQuestion");
 const startQuiz = document.getElementById("startQuiz");
 const text = document.getElementById("text");
 
 let error = "";
 let currentQuestion = 0;
+let timeLeft = 0
 
 const startQuestion = () => {
+  console.log("Yo")
   toDisplayQuestion.innerHTML = questions[currentQuestion].question;
   startQuiz.remove();
   text.textContent = "";
